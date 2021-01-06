159607fa52a98b724532c1cc882fc505d00acfcc
Amaranth T. Gear <69144534+Tigernauts@users.noreply.github.com>
2021-01-07 02:56:02 +0300

    Add correct Hex output

--- hex.js
+++ hex.js
@@ -1,7 +1,11 @@
 class HEX {
     even = n => !(n % 2); //проверка на четность
-    BADC = s => { return s.match(/(.{4})/g).map(part => part.match(/(.{2})/g).reverse().join("")).join(""); };
-    HexSpacer = s => {};
+    BADC = s => {
+        return s.match(/(.{4})/g).map(part => part.match(/(.{2})/g).reverse().join("")).join("");
+    };
+    DCBA = s => {
+        return s.match(/(.{8})/g).map(part => part.match(/(.{2})/g).reverse().join("")).join("");
+    };
     constructor(HexStringInput) {
         //Big Endian	(false)
         this.DCBAbuffer = null; //Little Endian (true)
@@ -31,11 +35,15 @@ class HEX {
         if (this._checkString()) { //проверка на валидность строки
             this._normalize();
             this._wordArray_ABCD = this._HexText.match(/(.{2})/g).map(
-                (el) => { return parseInt(el, 16); }
+                (el) => {
+                    return parseInt(el, 16);
+                }
             );
             this._HexText2 = this.BADC(this._HexText);
             this._wordArray_BADC = this._HexText2.match(/(.{2})/g).map(
-                (el) => { return parseInt(el, 16); }
+                (el) => {
+                    return parseInt(el, 16);
+                }
             );
             //мой метод дополнения нулей перед числом.
             //this._HexText="0".repeat((4-this._HexText.length%4)<4?(4-this._HexText.length%4):0)+this._HexText;
@@ -104,13 +112,19 @@ class HEX {
             this._HexText = "0" + this._HexText;
         }
         //добавление нулей для недостающих word
-        if (this._HexText.length % 4) { this._HexText += "00"; }
+        if (this._HexText.length % 4) {
+            this._HexText += "00";
+        }
         this._wordArray_ABCD = this._HexText.match(/(.{4})/g).map(
-            (el) => { return parseInt(el, 16); }
+            (el) => {
+                return parseInt(el, 16);
+            }
         );
         this.WordsCount = this._wordArray_ABCD.length;
         //добавление нулей для недостающих DWord
-        if (this._HexText.length % 8) { this._HexText += "0000"; }
+        if (this._HexText.length % 8) {
+            this._HexText += "0000";
+        }
 
     }
     _build() {
@@ -122,7 +136,10 @@ class HEX {
         for (let i = 0; i < this.WordsCount; i++) {
             arr[i] = this._view_ABCD.getUint16(i * 2, false);
         }
-        return { hex: this._HexText.match(/(.{4})/g), value: arr };
+        return {
+            hex: this._HexText.match(/(.{4})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///UINT16 - Little Endian (BA)
     get getUint16BA() {
@@ -130,7 +147,10 @@ class HEX {
         for (let i = 0; i < this.WordsCount; i++) {
             arr[i] = this._view_ABCD.getUint16(i * 2, true);
         }
-        return { hex: this.BADC(this._HexText).match(/(.{4})/g), value: arr };
+        return {
+            hex: this.BADC(this._HexText).match(/(.{4})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///INT16 - Big Endian (AB)
     get getInt16AB() {
@@ -138,7 +158,10 @@ class HEX {
         for (let i = 0; i < this.WordsCount; i++) {
             arr[i] = this._view_ABCD.getInt16(i * 2, false);
         }
-        return { hex: this._HexText.match(/(.{4})/g), value: arr };
+        return {
+            hex: this._HexText.match(/(.{4})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///INT16 - Little Endian (BA)
     get getInt16BA() {
@@ -146,7 +169,10 @@ class HEX {
         for (let i = 0; i < this.WordsCount; i++) {
             arr[i] = this._view_ABCD.getInt16(i * 2, true);
         }
-        return { hex: this.BADC(this._HexText).match(/(.{4})/g), value: arr };
+        return {
+            hex: this.BADC(this._HexText).match(/(.{4})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///UINT32 - Big Endian (ABCD)
     get getUint32ABCD() {
@@ -154,7 +180,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_ABCD.getUint32(i * 4, false);
         }
-        return { hex: this._HexText.match(/(.{8})/g), value: arr };
+        return {
+            hex: this._HexText.match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///UINT32 - Little Endian (DCBA)
     get getUint32DCBA() {
@@ -162,7 +191,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_ABCD.getUint32(i * 4, true);
         }
-        return { hex: this._HexText.match(/(.{8})/g), value: arr };
+        return {
+            hex: this.DCBA(this._HexText).match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///UINT32 - Mid-Big Endian (BADC)
     get getUint32BADC() {
@@ -170,7 +202,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_BADC.getUint32(i * 4, false);
         }
-        return { hex: this._HexText2.match(/(.{8})/g), value: arr };
+        return {
+            hex: this._HexText2.match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///UINT32 - Mid-Little Endian (CDAB)
     get getUint32CDAB() {
@@ -178,7 +213,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_BADC.getUint32(i * 4, true);
         }
-        return { hex: this._HexText2.match(/(.{8})/g), value: arr };
+        return {
+            hex: this.DCBA(this._HexText2).match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///INT32 - Big Endian (ABCD)
     get getInt32ABCD() {
@@ -186,7 +224,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_ABCD.getInt32(i * 4, false);
         }
-        return { hex: this._HexText.match(/(.{8})/g), value: arr };
+        return {
+            hex: this._HexText.match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///INT32 - Little Endian (DCBA)
     get getInt32DCBA() {
@@ -194,7 +235,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_ABCD.getInt32(i * 4, true);
         }
-        return { hex: this._HexText.match(/(.{8})/g), value: arr };
+        return {
+            hex: this.DCBA(this._HexText).match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///INT32 - Mid-Big Endian (BADC)
     get getInt32BADC() {
@@ -202,7 +246,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_BADC.getInt32(i * 4, false);
         }
-        return { hex: this._HexText2.match(/(.{8})/g), value: arr };
+        return {
+            hex: this._HexText2.match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///INT32 - Mid-Little Endian (CDAB)
     get getInt32CDAB() {
@@ -210,7 +257,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_BADC.getInt32(i * 4, true);
         }
-        return { hex: this._HexText2.match(/(.{8})/g), value: arr };
+        return {
+            hex: this.DCBA(this._HexText2).match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///Float32 - Big Endian (ABCD)
     get getFloat32ABCD() {
@@ -218,7 +268,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_ABCD.getFloat32(i * 4, false);
         }
-        return { hex: this._HexText.match(/(.{8})/g), value: arr };
+        return {
+            hex: this._HexText.match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///Float32 - Little Endian (DCBA)
     get getFloat32DCBA() {
@@ -226,7 +279,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_ABCD.getFloat32(i * 4, true);
         }
-        return { hex: this._HexText.match(/(.{8})/g), value: arr };
+        return {
+            hex: this.DCBA(this._HexText).match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///Float32 - Mid-Big Endian (BADC)
     get getFloat32BADC() {
@@ -234,7 +290,10 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_BADC.getFloat32(i * 4, false);
         }
-        return { hex: this._HexText2.match(/(.{8})/g), value: arr };
+        return {
+            hex: this._HexText2.match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
     ///Float32 - Mid-Little Endian (CDAB)
     get getFloat32CDAB() {
@@ -242,6 +301,9 @@ class HEX {
         for (let i = 0; i < this.DwordsCount; i++) {
             arr[i] = this._view_BADC.getFloat32(i * 4, true);
         }
-        return { hex: this._HexText2.match(/(.{8})/g), value: arr };
+        return {
+            hex: this.DCBA(this._HexText2).match(/(.{8})/g).map(part => part.match(/(.{2})/g).join(" ")),
+            value: arr
+        };
     }
 }
