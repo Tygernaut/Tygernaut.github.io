365fe2cba64081b835287da7e1ae1585920e110e
Amaranth T. Gear <69144534+Tigernauts@users.noreply.github.com>
2022-05-18 15:27:45 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,40 @@
+
+
+const data = [
+    {
+        "id": 1,
+        "name": "Keenetic",
+        "devices": [
+            {
+                "id": 1,
+                "model": "KN-1011",
+                "login": "admin",
+                "password": "no-password"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "name": "WAGO",
+        "devices": [
+            {
+                "id": 1,
+                "model": "750-881",
+                "login": "admin",
+                "password": "wago"
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "name": "DELL",
+        "devices": [
+            {
+                "id": 1,
+                "model": "iDRAC-12S77C3",
+                "login": "root",
+                "password": "calvin"
+            }
+        ]
+    }
+]
